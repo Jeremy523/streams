7f1ddfb3ed40c61c8a041a09ac506ac99b43158e
Jeremy De La Cruz <jdelacruz8876@bths.edu>
2016-04-23 17:41:27 +0000

    Add more comments

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Global dependencies
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,16 +6,21 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//Local dependencies
 var router = require('./routes');
 
+//Create app instance
 var app = express();
 
 // view engine setup
+// set the location of our views directory
 app.set('views', path.join(__dirname, 'views'));
+// set jade as the default view engine
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// middleware
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
